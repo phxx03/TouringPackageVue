acbdc6054efcbc5f22d8c48fe7c850cae71ede96
phxx03 <piyada.ph.62@ubu.ac.th>
2022-06-26 20:06:16 +0700

    06-26-22

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,68 +1,165 @@
 <template>
   <ion-page>
-    <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Blank</ion-title>
+    <ion-header class="header" mode="ios">
+      <ion-toolbar color="danger">
+        <ion-buttons slot="start">
+          <ion-back-button text="" :icon="chevronBackOutline"></ion-back-button>
+        </ion-buttons>
+        <ion-title class="text-light ion-padding-center">Touring Packge </ion-title>
       </ion-toolbar>
     </ion-header>
-    
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
-      <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-      </div>
-    </ion-content>
+
+  <ion-content color="tertiary">
+    <ion-searchbar mode="ios"></ion-searchbar>
+      <ion-card>
+        <ion-card-content color="tertiary">
+          <div class="ion-no-margin">
+            <ion-list v-for="pagkage in pagkages" :key="pagkage.image">
+              <ion-card color="tertiary" class="ion-no-padding border-radius">
+                      <ion-img :src="pagkage.image"></ion-img>
+                      <ion-grid>
+                        <ion-row>
+                          <ion-col size="7">
+                            <ion-label>
+                              <h2><b>{{pagkage.englishname}}</b></h2>
+                              <h3>{{pagkage.thainame}}</h3>
+                            </ion-label>
+                          </ion-col>
+                          <ion-col size="5">
+                            <ion-label>
+                              <h3 class="ion-text-right">
+                                <ion-text color="danger">
+                                  <h2>
+                                    {{pagkage.price}}<small>&nbsp;THB</small><span>/คน</span>
+                                  </h2>
+                                </ion-text>
+                                <ion-text color="medium">
+                                  ราคารวมภาษีแล้ว
+                                </ion-text>
+                              </h3>
+                            </ion-label>
+                          </ion-col>
+                        </ion-row>
+                      </ion-grid>
+                      <ion-card-content style="text-align: left;">
+                          <ion-icon :icon="busOutline"></ion-icon>&nbsp;{{pagkage.icon1}}<br>
+                          <ion-icon :icon="bedOutline"></ion-icon>&nbsp;{{pagkage.icon2}}<br>
+                          <ion-icon :icon="fastFoodOutline"></ion-icon>&nbsp;{{pagkage.icon3}}<br>
+                      </ion-card-content>
+                    <ion-button color="danger" class="ion-no-margin" expand="full" @click="() => router.push('/TourDetail')">เลือก</ion-button>
+              </ion-card>
+            </ion-list>
+          </div>
+        </ion-card-content>
+    </ion-card>
+  </ion-content>
+  
+
+
   </ion-page>
 </template>
 
-<script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+<script>
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButtons,
+  IonBackButton,
+  IonCard,
+  IonList,
+  IonItem,
+  IonInput,
+  IonButton,
+  IonLabel, 
+  IonTabBar, 
+  IonTabButton, 
+  IonTabs,
+  IonFooter,
+  IonIcon, 
+  IonCol,
+  IonGrid,
+  IonRow
+} from '@ionic/vue';
 import { defineComponent } from 'vue';
+import { busOutline,bedOutline,fastFoodOutline,alertCircleOutline,chevronBackOutline } from 'ionicons/icons';
+import { useRouter } from "vue-router";
 
 export default defineComponent({
   name: 'HomePage',
   components: {
-    IonContent,
-    IonHeader,
     IonPage,
+    IonHeader,
+    IonToolbar,
     IonTitle,
-    IonToolbar
+    IonContent,
+    IonButtons,
+    IonBackButton, 
+    IonCard,
+    IonIcon, 
+    IonCol,
+    IonGrid,
+    IonRow
+  },
+  setup() {
+    const router = useRouter();
+    
+    const pagkages = [
+    { 'thainame': 'ไร่เลย์ เบย์ รีสอร์ท แอนด์ สปา',
+     'englishname': 'Railay Bay Resort',
+     'icon1': 'รถรับส่งส่วนตัว',
+     'icon2': 'พักรีสอร์ทติดหาดติดหาดไร่เลย์ ',
+     'icon3': 'อาหารเช้าแบบบุฟเฟ่ต์ 2 มื้อ',
+     'image': 'https://itp1.itopfile.com/ImageServer/itp_17012020ybvl/0/0/Home-Recommend-3z-z877547007295.jpg',
+     'price': '2,590'} ,
+     {'thainame': 'เคซี แกรนด์ รีสอร์ท แอนด์ สปา',
+     'englishname': 'KC Grande Resort',
+     'icon1': 'รถรับส่งส่วนตัว',
+     'icon2': 'พักผ่อนติดเกาะหย่อนใจ',
+     'icon3': 'อาหารเช้าแบบบุฟเฟ่ต์ 2 มื้อ',
+     'image': 'https://itp1.itopfile.com/ImageServer/itp_17012020ybvl/1610/0/Home-Recommend-1z-z1094936940018.jpg',
+     'price': '3,390'} ,
+     {'thainame': 'ทรายแก้ว บีช รีสอร์ท',
+     'englishname': 'Sai Keaw Beach Resort',
+     'icon1': 'รถรับส่งส่วนตัว',
+     'icon2': 'พักรีสอร์ทติดหาดติดหาดไร่เลย์ ',
+     'icon3': 'พร้อมอาหารเช้าแบบบุฟเฟ่ต์ 2 มื้อ',
+     'image': 'https://itp1.itopfile.com/ImageServer/itp_17012020ybvl/0/0/Home-Recommend-3z-z877547007295.jpg',
+     'price': '2,590'} ,
+     {'thainame': 'โนโวเทล ระยอง ริมแพ รีสอร์ท',
+     'englishname': 'NovotelRayong Rimpae',
+     'icon1': 'รถรับส่งส่วนตัว',
+     'icon2': 'พักรีสอร์ทติดหาดติดหาดไร่เลย์ ',
+     'icon3': 'พร้อมอาหารเช้าแบบบุฟเฟ่ต์ 2 มื้อ',
+     'image': 'https://itp1.itopfile.com/ImageServer/itp_17012020ybvl/526/0/Novotel-19-5-65z-z257763485456.jpg',
+     'price': '1,145'}
+    ];
+    
+    return {
+      router,
+      busOutline,
+      bedOutline,
+      fastFoodOutline,
+      alertCircleOutline,
+      chevronBackOutline,
+      pagkages
+    }
   }
 });
 </script>
 
 <style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
+.cardpromo {
+  height: 300px;
 }
-
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
+.imgpromo {
+  height: 220px;
 }
-
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  
-  color: #8c8c8c;
-  
-  margin: 0;
-}
-
-#container a {
-  text-decoration: none;
+ion-searchbar {
+  margin: auto;
+  width: 100%;
+  border-radius: 1px;
 }
 </style>
---
+++ src/views/TourDetail.vue
@@ -0,0 +1,176 @@
+<template>
+  <ion-page>
+    <ion-header class="header" mode="ios">
+  <ion-toolbar color="danger">
+    <ion-buttons slot="start">
+      <ion-back-button text="" :icon="chevronBackOutline"></ion-back-button>
+    </ion-buttons>
+    <ion-title class="text-light ion-padding-center">Touring Detail </ion-title>
+  </ion-toolbar>
+</ion-header>
+
+<ion-content>
+    <ion-card color="tertiary">
+      <div class="ion-no-margin">
+          <ion-list lines="full" class="ion-no-padding">
+              <ion-slides>
+                <ion-slide>
+                  <div class="slide">
+                    <img
+                      src="https://itp1.itopfile.com/ImageServer/itp_17012020ybvl/803/0/RailayBay-1200x8000038img-6z-z429956806395.jpg"
+                    />
+                  </div>
+                </ion-slide>
+
+                <ion-slide>
+                  <img
+                    src="https://itp1.itopfile.com/ImageServer/itp_17012020ybvl/0/0/RailayBay-1200x8000013Deluxecottage-1z-z521229803942.jpg"
+                  />
+                </ion-slide>
+
+                <ion-slide>
+                  <img
+                    src="https://itp1.itopfile.com/ImageServer/itp_17012020ybvl/0/0/RailayBay-1200x8000044img-1(2)z-z963804355974.jpg"
+                  />
+                </ion-slide>
+
+                <ion-slide>
+                  <img
+                    src="https://itp1.itopfile.com/ImageServer/itp_17012020ybvl/0/0/3RailayBay-1200x800z-z1401813691806.jpg"
+                  />
+                </ion-slide>
+              </ion-slides>
+
+              <ion-grid>
+                <ion-row>
+                  <ion-col size="7">
+                    <ion-label>
+                      <h2><b>Railay Bay Resort</b></h2>
+                      <h3>ไร่เลย์ เบย์ รีสอร์ท แอนด์ สปา</h3>
+                    </ion-label>
+                  </ion-col>
+                  <ion-col size="5">
+                    <ion-label>
+                      <h3 class="ion-text-right">
+                        <ion-text color="danger">
+                          <h2>
+                            2,590<small>&nbsp;THB</small><span>/คน</span>
+                          </h2>
+                        </ion-text>
+                        <ion-text color="medium"> ราคารวมภาษีแล้ว </ion-text>
+                      </h3>
+                    </ion-label>
+                  </ion-col>
+                </ion-row>
+              </ion-grid>
+
+              <ion-card-content style="text-align: left">
+                <h2><b>รายละเอียดงานบริการ</b></h2>
+                <ion-icon :icon="busOutline"></ion-icon
+                >&nbsp;รถรับส่งส่วนตัว<br />
+                <ion-icon :icon="bedOutline"></ion-icon
+                >&nbsp;พักรีสอร์ทติดหาดติดหาดไร่เลย์<br />
+                <ion-icon :icon="fastFoodOutline"></ion-icon
+                >&nbsp;อาหารเช้าแบบบุฟเฟ่ต์ 2 มื้อ<br />
+              </ion-card-content>
+            <ion-accordion-group color="tertiary" mode="ios">
+            <ion-accordion value="start" color="tertiary" toggle-icon="arrow-down-circle" >
+              <ion-item slot="header" color="tertiary">
+              <ion-icon color="danger" :icon="alertCircleOutline"></ion-icon>
+                <ion-label color="danger" style="font-size: 15px;">&nbsp;&nbsp;หมายเหตุ การใช้งานแพ็คเกจ</ion-label>
+                <ion-chip color="danger"> <ion-label color="danger">อ่านเพิ่ม</ion-label></ion-chip>
+              </ion-item>
+              <ion-card-header slot="content" color="tertiary">
+              <ion-card-subtitle >
+                  - ท่านสามารถเลือกวันเข้าพักได้ทุกวัน<br />
+                  - โปรโมชั่น 3 วัน 2 คืน สำหรับจอง 2 ท่าน ขึ้นไป<br />
+                  - เลือกจองเข้าพักได้ วันที่ 1 พ.ค. ถึง 31 ต.ค. 65 ชำระท่านละ
+                  2,590 บ.<br />
+                  - สำหรับ เด็กอายุ 4 - 11 ปี เสริมเตียง ชำระท่านละ 2,590 บ.<br />
+                  - สำหรับ เด็กอายุ 4 - 11 ปี พักกับผู้ปกครอง ชำระท่านละ 490
+                  บ.<br />
+                  - สำหรับ เด็กอายุ 0 - 3 ปี ไม่เสียค่าใช้จ่าย<br />
+                  - โปรโมชั่น แพ็คเกจนี้
+                  สงวนสิทธิ์สำหรับผู้ซื้อและระบุวันเดินทางภายใน 30 มิ.ย. 65
+                  เท่านั้น<br />
+                  - กรณี ไม่สามารถระบุวันเดินทางภายในวันที่กำหนด
+                  สามารถปรับเปลี่ยนเป็นแพ็คเกจอื่นได้ฟรี <br />
+                  - หากต้องการแพ็คเกจเดิม
+                  ราคาแพ็คเกจอาจมีการเปลี่ยนแปลงตามแพ็คเกจโปรโมชั่น ณ ขณะนั้น<br />
+                  - VOUCHER ไม่สามารถเปลี่ยนแปลงเป็นเงินสดได้<br />
+                  - ติดต่อเพื่อเช็คห้องว่างล่วงหน้า 14 - 21 วัน<br />
+                  - อายุการใช้งาน โปรโมชั่น แพ็คเกจ : 31 ต.ค. 2565<br />
+                </ion-card-subtitle>
+                </ion-card-header>
+            </ion-accordion>
+          </ion-accordion-group>
+          </ion-list>
+
+          <br/>
+          <ion-button color="danger" class="ion-no-margin" expand="full" @click="() => router.push('/TourDetail')">เลือก</ion-button>
+      </div>
+  </ion-card>
+</ion-content>
+  
+
+
+  </ion-page>
+</template>
+
+<script>
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonButtons,
+  IonBackButton,
+  IonCard,
+  IonList,
+  IonItem,
+  IonInput,
+  IonButton,
+  IonLabel, 
+  IonTabBar, 
+  IonTabButton, 
+  IonTabs,
+  IonFooter,
+  IonIcon, 
+  IonCol,
+  IonGrid,
+  IonRow
+} from '@ionic/vue';
+import { defineComponent } from 'vue';
+
+import { busOutline,bedOutline,fastFoodOutline,alertCircleOutline,chevronBackOutline } from 'ionicons/icons';
+import { useRouter } from 'vue-router';
+
+export default defineComponent({
+  name: 'HomePage',
+  components: {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonButtons,
+    IonBackButton, 
+    IonCard,
+    IonIcon, 
+    IonCol,
+    IonGrid,
+    IonRow
+  },
+    setup() {
+      return {
+        router: useRouter(),
+        busOutline,
+        bedOutline,
+        fastFoodOutline,
+        alertCircleOutline,
+        chevronBackOutline,
+    };
+    }
+});
+</script>
